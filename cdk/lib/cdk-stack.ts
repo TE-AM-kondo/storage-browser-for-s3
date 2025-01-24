5d3eeabf492ac4f8054f51db59217d494ba1505e
Akira Kawabata <kawabata@skyarch.net>
2025-01-25 04:22:34 +0900

    add cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,40 +5,40 @@ import { Cognito } from './constructs/cognito';
 import { Iam } from './constructs/iam';
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: cdk.StackProps) {
+        super(scope, id, props);
 
-        // S3リソースを作成
-        const s3 = new S3(this, 'S3', {
-          ...props,
-      });
+            // S3リソースを作成
+            const s3 = new S3(this, 'S3', {
+            ...props,
+        });
 
-      // Cognitoリソースを作成
-      const cognito = new Cognito(this, 'Cognito', {
-          ...props,
-          bucketName: s3.bucket.bucketName,
-      });
+        // Cognitoリソースを作成
+        const cognito = new Cognito(this, 'Cognito', {
+            ...props,
+            bucketName: s3.bucket.bucketName,
+        });
 
-      // IAMリソースを作成
-      const iam = new Iam(this, 'IAM', {
-          ...props,
-          bucketName: s3.bucket.bucketName,
-          identityPool: cognito.identityPool,
-      });
+        // IAMリソースを作成
+        const iam = new Iam(this, 'IAM', {
+            ...props,
+            bucketName: s3.bucket.bucketName,
+            identityPool: cognito.identityPool,
+        });
 
-      // S3バケットに認証済みロールのポリシーを追加
-      s3.addAuthenticatedRolePolicy(iam.authenticatedRole);
+        // S3バケットに認証済みロールのポリシーを追加
+        s3.addAuthenticatedRolePolicy(iam.authenticatedRole);
 
-      // Amplify設定をJSON形式で出力
-      const amplifyOutputs = {
-          auth: cognito.getCognitoAmplifyConfig(),
-          storage: s3.getS3AmplifyConfig(),
-          version: '1.3',
-      };
+        // Amplify設定をJSON形式で出力
+        const amplifyOutputs = {
+            auth: cognito.getCognitoAmplifyConfig(),
+            storage: s3.getS3AmplifyConfig(),
+            version: '1.3',
+        };
 
-      // Amplify設定を出力
-      new cdk.CfnOutput(this, 'AmplifyOutputs', {
-          value: JSON.stringify(amplifyOutputs),
-      });
-  }
+        // Amplify設定を出力
+        new cdk.CfnOutput(this, 'AmplifyOutputs', {
+            value: JSON.stringify(amplifyOutputs),
+        });
+    }
 }
